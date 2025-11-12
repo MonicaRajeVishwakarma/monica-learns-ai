69b9d277ca8b5eed10354f3c81a122438c407034
MonicaRajeVishwakarma <43462756+MonicaRajeVishwakarma@users.noreply.github.com>
2025-11-12 11:19:10 +0100

    Added one more variables of all types

--- day-01-python-basics/exercises.py
+++ day-01-python-basics/exercises.py
@@ -2,25 +2,44 @@
 
 # 1. Create variables
 your_name = "Monica"
+your_lastname = "Vishwakarma"
 baby_age_weeks = 14
+baby_age_months = 4
 milk_quantity = 0.12
+water_quantity = 0.01
 is_sleeping = True
+is_rolling = False
 
 # 2. Print their types
 print(type(your_name))
+print(type(your_lastname))
 print(type(baby_age_weeks))
+print(type(baby_age_months))
 print(type(milk_quantity))
+print(type(water_quantity))
 print(type(is_sleeping))
+print(type(is_rolling))
+
 
 # 3. Arithmetic practice
-future_age = baby_age_weeks + 2
-print("Baby's age in 2 weeks:", future_age)
+future_age_weeks = baby_age_weeks + 2
+print("Baby's age in 2 weeks:", future_age_weeks)
+future_age_months = baby_age_months + 2
+print("Baby's age in 2 months",future_age_months)
+
 
 double_milk = milk_quantity * 2
 print("Double milk quantity:", double_milk)
 
+double_water = water_quantity * 2
+print("Double water quantity",double_water)
+
 # 4. String operations
-msg = your_name + " is learning AI!"
+msg = your_name + " " + your_lastname +" is learning AI!"
 print(msg)
 
+
 print("Keep going! " * 3)
+print("Keep learning! " * 3)
+
+
