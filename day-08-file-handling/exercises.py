c819839ddc292d1a350468b4a34c7bfffa63a87b
MonicaRajeVishwakarma <43462756+MonicaRajeVishwakarma@users.noreply.github.com>
2025-11-19 23:28:30 +0100

    exercises for file handling

---
+++ day-08-file-handling/exercises.py
@@ -0,0 +1,22 @@
+# Day 08 - File handling
+
+# 1. Write to a file
+with open("baby_log.txt","w") as file:
+    file.write("Day 1 : Leo smiled today!\n")
+    file.write("Day 2 : Leo slept 6 hours.\n")
+
+
+# 2. Read from a file
+with open("baby_log.txt","r") as file:
+    print("Reading log")
+    print(file.read())
+
+#3. Append to a file
+with open("baby_log.txt","a") as file:
+    file.write("Day 3: Leo tried rolling over!\n")
+
+# 4️⃣ Read line by line
+with open("baby_log.txt", "r") as file:
+    print("\nReading line by line:")
+    for line in file:
+        print(line.strip())
