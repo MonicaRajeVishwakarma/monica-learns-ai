b8274c5ec30e40cb344f586ca7fa870a018cb8ef
MonicaRajeVishwakarma <43462756+MonicaRajeVishwakarma@users.noreply.github.com>
2025-11-26 23:56:03 +0100

    Add files for numpy basic

---
+++ day-09-numpy/exercises.py
@@ -0,0 +1,13 @@
+# Day 09 - NumPy Basics
+
+import numpy as np
+
+#1. Create a 1D array from 0 to 9
+arr = np.arange(10)
+print("iD Array:",arr)
+
+#2. Create a 3*3 array of ones
+ones = np.ones((3,3))
+print("3*3 ones\n",ones)
+
+#3. Element wise operation
