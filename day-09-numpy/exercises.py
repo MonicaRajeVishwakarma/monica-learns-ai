b7cad17e7d9e63c9db16835359d1311870af63c7
MonicaRajeVishwakarma <43462756+MonicaRajeVishwakarma@users.noreply.github.com>
2025-11-28 00:52:56 +0100

    exercises numpy basic

--- day-09-numpy/exercises.py
+++ day-09-numpy/exercises.py
@@ -8,6 +8,18 @@ print("iD Array:",arr)
 
 #2. Create a 3*3 array of ones
 ones = np.ones((3,3))
-print("3*3 ones\n",ones)
+print("\n3*3 ones\n",ones)
 
 #3. Element wise operation
+arr2 = arr * 2
+print("\nArray multiplied by 2",arr2)
+
+#4. Aggregations
+print("\nSum of arr",arr.sum())
+print("\nMean of arr",arr.mean())
+
+#5. Indexing
+arr2d = np.array([[1,2,3],[4,5,6]])
+print("\n2D array:\n",arr2d)
+print("\nElement at row0, col2:",arr2d[0,2])
+print("\nColumn 1:",arr2d[:,1])
