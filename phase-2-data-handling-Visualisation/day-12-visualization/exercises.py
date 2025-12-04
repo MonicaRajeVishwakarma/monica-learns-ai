6c05a56ffc38f2c782966ea060e417342f9a3364
MonicaRajeVishwakarma <43462756+MonicaRajeVishwakarma@users.noreply.github.com>
2025-12-04 17:49:34 +0100

    refactoring

--- phase-2-data-handling-Visualisation/day-12-visualization/exercises.py
+++ phase-2-data-handling-Visualisation/day-12-visualization/exercises.py
@@ -5,7 +5,7 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 
 # Load dataset
-df = pd.read_csv("../data/habits.csv")
+df = pd.read_csv("../../data/habits.csv")
 
 # Clean sleep column (if needed)
 if 'sleep' in df.columns:
